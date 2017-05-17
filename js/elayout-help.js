ba623abf15f0be3ffc0f7324aa3a35e8e00d2f1c
Abhilash Asokan <abhilash@heidelsoft-technologies.de>
2017-05-17 16:07:51 +0530

    First commit

---
+++ js/elayout-help.js
@@ -0,0 +1,45 @@
+$(function() {
+    $(".docs-nav").load("partial/menu.html");
+    $(".docs-nav").show();
+
+    //load welcome text to span.welcome
+    $(".span-welcome").load("partial/client/welcome.html");
+    //load application usage text to span-application-usage
+    $(".span-application-usage").load("partial/client/application-usage.html");
+    //load application work flow text to span-application-usage
+    $(".span-application-workflow").load("partial/client/application-workflow.html");
+    //load application editor text to span-application-editor
+    $(".span-application-editor").load("partial/client/application-editor.html");
+    //load editor controls text to span-editor-controls
+    $(".span-editor-controls").load("partial/client/editor-controls.html");
+    //load editor controls html-div text to span-editor-controls-html-div
+    $(".span-editor-controls-html-div").load("partial/client/editor-controls-html-div.html");
+    //load editor controls html-image text to span-editor-controls-html-image
+    $(".span-editor-controls-html-image").load("partial/client/editor-controls-html-image.html");
+    //load editor controls html-ul text to span-editor-controls-html-ul
+    $(".span-editor-controls-html-ul").load("partial/client/editor-controls-html-ul.html");
+    //load editor controls html-ol text to span-editor-controls-html-ol
+    $(".span-editor-controls-html-ol").load("partial/client/editor-controls-html-ol.html");
+    //load editor controls html-span text to span-editor-controls-html-span
+    $(".span-editor-controls-html-span").load("partial/client/editor-controls-html-span.html");
+    //load editor controls cs-align text to span-editor-controls-cs-align
+    $(".span-editor-controls-cs-align").load("partial/client/editor-controls-cs-align.html");
+    //load editor controls cs-article text to span-editor-controls-cs-article
+    $(".span-editor-controls-cs-article").load("partial/client/editor-controls-cs-article.html");
+    //load editor controls cs-block text to span-editor-controls-cs-block
+    $(".span-editor-controls-cs-block").load("partial/client/editor-controls-cs-block.html");
+    //load editor controls cs-image text to span-editor-controls-cs-image
+    $(".span-editor-controls-cs-image").load("partial/client/editor-controls-cs-image.html");
+    //load editor controls cs-inline imagetext to span-editor-controls-cs-inline
+    $(".span-editor-controls-cs-inline").load("partial/client/editor-controls-cs-inline.html");
+    //load editor controls cs-mirror text to span-editor-controls-cs-mirror
+    $(".span-editor-controls-cs-mirror").load("partial/client/editor-controls-cs-mirror.html");
+    //load editor controls cs-modules text to span-editor-controls-cs-modules
+    $(".span-editor-controls-cs-modules").load("partial/client/editor-controls-cs-modules.html");
+    //load editor controls cs-select text to span-editor-controls-cs-select
+    $(".span-editor-controls-cs-select").load("partial/client/editor-controls-cs-select.html");
+    //load editor controls cs-address text to span-editor-controls-cs-address
+    $(".span-editor-controls-cs-address").load("partial/client/editor-controls-cs-address.html");
+    //load faq text to span-faq
+    $(".span-faq").load("partial/client/faq.html");
+});
